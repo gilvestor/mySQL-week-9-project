146c373f5567e57921b157f843559fbf60fefbcc
Gibert Wanyoike <gilvestor1@gmail.com>
2022-09-08 22:44:37 -0400

    first commit

--- src/main/java/projects/dao/DBConnection.java
+++ src/main/java/projects/dao/DBConnection.java
@@ -3,17 +3,15 @@ package projects.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-import projects.exception.DBException;
 import projects.exception.DbException;
 
 public class DBConnection {
 
 	public static final String HOST = "localhost";
-	public static final String PASSWORD = "projects";
+	public static final String PASSWORD = "projects_schema";
 	public static final int PORT = 3306;
-	public static final String SCHEMA = "projects";
-	public static final String USER = "projects";
+	public static final String SCHEMA = "projects_schema";
+	public static final String USER = "projects_schema";
 
 	public static Connection getConnection() {
 		String url = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false", HOST, PORT, SCHEMA, USER,
